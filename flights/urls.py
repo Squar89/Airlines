7b0abb87974cb3e32ba157149a6b33836d9b4886
Jakub Wróblewski <j.wroblewski97@gmail.com>
2018-05-18 02:55:18 +0200

    Implemented logging in

--- flights/urls.py
+++ flights/urls.py
@@ -1,4 +1,6 @@
-from django.urls import path
+from django.urls import path, re_path
+from django.contrib.auth import views as auth_views
+from django.views.generic import RedirectView
 
 from . import views
 
@@ -7,4 +9,6 @@ app_name = 'flights'
 urlpatterns = [
     path('', views.home, name='home'),
     path('<int:flight_id>/', views.detail, name='detail'),
+    path('login', auth_views.login, name='login'),
+    re_path(r'^logout/$', auth_views.logout, {'next_page': '/flights'}, name='logout')
 ]
--- flights/views.py
+++ flights/views.py
@@ -3,15 +3,15 @@ from .models import Flight
 
 
 def home(request):
-    flights_sorted = Flight.objects.order_by('date_dep')
+    flights_sorted = Flight.objects.order_by('date_dep')[:30]
     context = {
-        'flights_list': flights_sorted
+        'flights_list': flights_sorted,
     }
     return render(request, 'flights/home.html', context)
 
 
 def detail(request, flight_id):
     context = {
-        'flight': Flight.objects.get(id=flight_id)
+        'flight': Flight.objects.get(id=flight_id),
     }
     return render(request, 'flights/detail.html', context)
